5b7ee9cd562950dcb311d92c9803957b114c80d8
orjwan98 <orjwan98@hotmail.com>
2019-03-06 21:02:38 +0200

    make basic react app #1

---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return <div className="App">React is up and running</div>;
+  }
+}
+
+export default App;
