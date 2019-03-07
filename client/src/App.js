552dbacfb20d0345ac508ac4873be903df482f47
orjwan98 <orjwan98@hotmail.com>
2019-03-07 10:58:23 +0200

    add route and register component

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,16 @@
 import React, { Component } from "react";
+import { BrowserRouter, Route, Redirect } from "react-router-dom";
+import Register from "./Components/Register";
 
 class App extends Component {
   render() {
-    return <div className="App">React is up and running</div>;
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Route path="/register" render={props => <Register {...props} />} />
+        </div>
+      </BrowserRouter>
+    );
   }
 }
 
