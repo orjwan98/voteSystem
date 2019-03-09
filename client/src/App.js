6f98121f80be787dd03285c83011215aa7a95038
orjwan98 <orjwan98@hotmail.com>
2019-03-09 23:20:37 +0200

    add Home component to be rendered in App

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from "react";
 import { BrowserRouter, Route, Redirect } from "react-router-dom";
 import Register from "./Components/Register";
 import Login from "./Components/Login";
+import Home from "./Components/Home";
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div className="App">
+          <Route exact path="/" render={props => <Home {...props} />} />
           <Route path="/register" render={props => <Register {...props} />} />
           <Route path="/login" render={props => <Login {...props} />} />
         </div>
