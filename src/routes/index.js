95bf857c45ddf28f76feed4eed648433ec2a6416
orjwan98 <orjwan98@hotmail.com>
2019-03-09 23:23:59 +0200

    make home route, checkCookie middleware, and getData query which gets all records from posts

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,8 +2,11 @@ const express = require("express");
 const router = express.Router();
 const register = require("./register.js");
 const login = require("./login.js");
+const checkCookie = require("../middleWare/checkCookie");
+const getData = require("./getData");
 
-router.post("/register", register.post);
-router.post("/login", login.post);
+router.post("/register", register);
+router.post("/login", login);
+router.get("/home", checkCookie, getData);
 
 module.exports = router;
