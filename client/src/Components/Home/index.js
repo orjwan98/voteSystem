ab8914dd6b9b010452c3f0e92299e9fee716e297
orjwan98 <orjwan98@hotmail.com>
2019-03-09 23:26:38 +0200

    Make Home component

---
+++ client/src/Components/Home/index.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import axios from "axios";
+import Post from "../Post";
+
+class Home extends Component {
+  state = {
+    logged: null,
+    allPosts: null
+  };
+
+  componentDidMount() {
+    axios.get("/home").then(res => {
+      const resStatus = res.data;
+      if (!resStatus.logged) {
+        this.setState({ logged: resStatus });
+        const { history } = this.props;
+        history.push("/login");
+      } else {
+        this.setState(
+          {
+            logged: resStatus.logged,
+            allPosts: resStatus.allPosts
+          },
+          () => {
+            console.log(this.state);
+          }
+        );
+      }
+    });
+  }
+  render() {
+    const { allPosts } = this.state;
+    const posts = allPosts
+      ? allPosts.map(element => (
+          <Post key={element.id} title={element.title} body={element.body} />
+        ))
+      : "Loading...";
+
+    const home = this.state.logged ? (
+      <React.Fragment>{posts}</React.Fragment>
+    ) : null;
+
+    return <React.Fragment>{home}</React.Fragment>;
+  }
+}
+
+export default Home;
