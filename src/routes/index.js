b34508a15a6de8d8dcd7dec779a3302be2b8301e
orjwan98 <orjwan98@hotmail.com>
2019-03-12 15:04:19 +0200

    src/routes/index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,15 +4,14 @@ const register = require("./register.js");
 const login = require("./login.js");
 const checkCookie = require("../middleWare/checkCookie");
 const getData = require("./getData");
-const upVote = require("./upVote");
+const vote = require("./vote");
 const getUpVotes = require("./getUpVotes");
 const getDownVotes = require("./getDownVotes");
 
 router.post("/register", register);
 router.post("/login", login);
-router.post("/upVote", upVote);
+router.post("/vote", vote);
 router.get("/home", checkCookie, getData);
 router.get("/getUpVotes", getUpVotes);
 router.get("/getDownVotes", getDownVotes);
-
 module.exports = router;
